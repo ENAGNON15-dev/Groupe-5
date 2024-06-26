face30144e54cf6b5f13ef9edb2d39fd2aa64d23
Lionel Nahou <nahoulionel@gmail.com>
2024-06-26 14:26:27 +0100

    Ajout de l'histogramme en Python

---
+++ task 2/main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Charger les données
+data = pd.read_csv('dataset/Housing.csv')
+
+
+while True:
+    print("\n1. Histogramme \n2. Graphique de dispersion\n\t")
+    choice = input("Votre choix : ")
+
+    if choice == '1':
+        # Histogramme pour la colonne 'bedrooms'
+
+        plt.figure(figsize=(10, 6))
+        plt.hist(data['bedrooms'], bins=15, color='blue', edgecolor='black')
+        plt.title('Histogramme des chambres à coucher')
+        plt.xlabel('Nombre de chambres')
+        plt.ylabel('Fréquence')
+        plt.grid(True)
+        plt.show()
+
+        break
+    elif choice == '2':
+        # Graphique de dispersion pour 'area' et 'price'
+
+
+        break
+    else:
+        continue
